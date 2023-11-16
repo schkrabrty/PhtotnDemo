6b53b4b5b247af97845007879c906d151fe263cd
980417frank <yuanhao.lyu@vanderbilt.edu>
2023-11-16 12:12:37 -0600

    as

--- Assets/Scripts/networkPlayerSpawner.cs
+++ Assets/Scripts/networkPlayerSpawner.cs
@@ -11,7 +11,7 @@ public class networkPlayerSpawner : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         base.OnJoinedRoom();
-        spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player", transform.position, transform.rotation);
+        spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player3", transform.position, transform.rotation);
     }
 
     public override void OnLeftRoom()
---
+++ Assets/asd.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.InputSystem;
+
+public class asd : MonoBehaviour
+{
+    public InputActionProperty leftPrimaryButton;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
